3fd639a19bd43f37455c7d75e813e07aaf54b593
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-02-06 20:26:53 -0800

    commit tree c7e59f3fdbe1a52744026db2ef07f27f1217c9e7

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Add your Circle class here
+class Circle {
+
+    constructor(radius) {
+        this.radius = radius
+    }
+    
+    set diameter(diameter) {
+        this.radius = diameter / 2
+    }
+
+    set circumference(circumference) {
+        this.diameter = circumference / Math.PI
+    }
+
+    set area(area) {
+        this.radius = Math.sqrt(area / Math.PI)
+    }
+
+    get diameter() {
+        return this.diameter = this.radius * 2
+    }
+
+    get circumference() {
+        return this.circumference = this.diameter * Math.PI
+    }
+
+    get area() {
+        return this.area = Math.PI * (this.radius * this.radius)
+    }
+}
+
+const circle = new Circle(10)
+
+console.log('radius expect 10')
+console.log("=>", circle.radius)
+console.log('diameter expect 20')
+console.log("=>", circle.diameter)
+console.log('circle diameter set to 100')
+console.log("diameter =>", circle.diameter = 50)
+console.log('radius expect 25')
+console.log("=>", circle.radius)
+
